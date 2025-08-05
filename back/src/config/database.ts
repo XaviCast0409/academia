76fe9d35496d0638f133bdb2ec8b9a8f56a9aad8
XaviCast0409 <168607424+XaviCast0409@users.noreply.github.com>
2025-08-04 19:51:07 -0500

    new back app react antive

--- back/src/config/database.ts
+++ back/src/config/database.ts
@@ -1,21 +1,30 @@
 import { Sequelize } from "sequelize";
 import dataConfig from "./environment";
-import fs from "fs";
-import path from "path";
 import { VerificationCode } from "../models/VerificationCode";
+import { User } from "../models/User";
+import { Activity } from "../models/Activity";
+import { Evidence } from "../models/Evidence";
+import { Achievement } from "../models/Achievement";
+import { UserAchievement } from "../models/UserAchievement";
+import { Mission } from "../models/Mission";
+import { UserMission } from "../models/UserMission";
+import { Role } from "../models/Role";
+import { Pokemon } from "../models/Pokemon";
+import { Product } from "../models/Product";
+import { Transaction } from "../models/Transaction";
 
 const db: any = {}
-/* export const sequelize = new Sequelize({
+export const sequelize = new Sequelize({
   username: dataConfig.development.username,
   password: dataConfig.development.password,
   database: dataConfig.development.database,
   host: dataConfig.development.host,
   dialect: "postgres",
   logging: false,
-}); */
+});
 
 // configuracion produccion de la base de datos
-export const sequelize = new Sequelize(dataConfig.production.url, {
+/* export const sequelize = new Sequelize(dataConfig.production.url, {
   dialect: "postgres",
   logging: false,
   dialectOptions: {
@@ -24,27 +33,35 @@ export const sequelize = new Sequelize(dataConfig.production.url, {
       rejectUnauthorized: false, // Cambia esto según tus necesidades de seguridad
     },
   },
+}); */
+
+// Inicializar todos los modelos manualmente
+const models = [
+  User,
+  Activity,
+  Evidence,
+  Achievement,
+  UserAchievement,
+  Mission,
+  UserMission,
+  Role,
+  Pokemon,
+  Product,
+  Transaction,
+  VerificationCode
+];
+
+// Inicializar modelos
+models.forEach((model) => {
+  if (typeof model.initModel === "function") {
+    model.initModel(sequelize);
+  }
+  if (model.name) {
+    db[model.name] = model;
+  }
 });
 
-const modelsDir = path.join(__dirname, "../models");
-
-fs.readdirSync(modelsDir)
-  .filter((file: string) => {
-    return (
-      file.indexOf(".") !== 0 && file.slice(-3) === ".js" && !file.includes("index")
-    );
-  })
-  .forEach((file: string) => {
-    const modelModule = require(path.join(modelsDir, file));
-    const model = modelModule.default || modelModule;
-    if (typeof model.initModel === "function") {
-      model.initModel(sequelize); // Usar initModel
-    }
-    if (model.name) {
-      db[model.name] = model;
-    }
-  });
-
+// Configurar asociaciones
 Object.keys(db).forEach((modelName) => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
@@ -59,9 +76,6 @@ Object.keys(db).forEach((modelName) => {
     console.error("Error al conectar a la base de datos:", error);
   }
 })();
-
-// Add VerificationCode to db object
-db.VerificationCode = VerificationCode;
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
